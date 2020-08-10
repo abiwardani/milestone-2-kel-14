2ee4cadc58ad582f6a155410179f27b5216acfa9
davidmoelia <syafiqfaray2@gmail.com>
2020-08-10 23:55:03 +0700

    penambahan index.js untuk navigasi

---
+++ Index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator} from '@react-navigation/stack';
+
+import HomeScreen from './HomeScreen' ;
+import DiaryScreen from './DiaryScreen';
+import ProfileScreen from './ProfileScreen';
+
+const Stack = createStackNavigator();
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home" >
+          <Stack.Screen name='Home' component={HomeScreen} options={{ headerShown: false }}/>
+          <Stack.Screen name='Diary' component={DiaryScreen} options={{ headerShown: false }}/>
+          <Stack.Screen name='Profile' component={ProfileScreen} options={{ headerShown: false }}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
